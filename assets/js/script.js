c1ef03322d605c809d6c1916df28d2a96cab311f
Eli Pagan <mbk8689@gmail.com>
2021-11-05 12:29:00 -0400

    load and save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,44 @@
+function updateTimeSlotItems() {
+// assign classes (past | present | future)
+   
+
+    // read relevant local storage data (about the relevant hour slot)
+function loadDecscription(){
+    var value = $(e.target).siblings(".description").val();
+
+
+value = JSON.parse(localStorage.getItem(".description"));
+
+} $(".saveBtn").on('click', loadDecscription);
+};
+
+
+function handleSave(e) {
+    
+    var hour = $(e.target).closest(".time-block").attr('id');
+
+    var value = $(e.target).siblings(".description").val();
+    
+    
+    
+    localStorage.setItem(hour, value);
+}
+
+
+
 // display current date at top of calendar
 function day(){
-$("#currentDay").text(moment().format('dddd, LL'));
-}
-$(day);
+
+
+     $("#currentDay").text(moment().format("MMMM Do, YYYY - hh:mm:ss a"))
+
+     setInterval(function() {
+        $("#currentDay").each(function() {
+          day($(this));
+        });
+      }, 1000);
+
+      $(".saveBtn").on('click', handleSave);
+
+           
+};  day()
